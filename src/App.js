99b20e314dc16ab861fa4f2554040816898713cb
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-10-19 14:58:28 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/Inicio";
+import Menu from "./componentes/Menu";
+import SobreMim from "./paginas/SobreMim";
+
+
+function App() {
+  return ( 
+    <BrowserRouter>
+
+      <Menu />
+
+
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div><h2>Página não encontrada</h2></div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
