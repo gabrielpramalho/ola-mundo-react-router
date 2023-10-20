37d007291935c09870a7e42dc3db47f83dae323f
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-10-19 21:52:15 -0300

    add footer

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import Menu from "./componentes/Menu";
 import SobreMim from "./paginas/SobreMim";
+import Rodape from "componentes/Rodape/imdex";
 
 
 function AppRoutes() {
@@ -16,6 +17,8 @@ function AppRoutes() {
         <Route path="/sobremim" element={<SobreMim />} />
         <Route path="*" element={<div><h2>Página não encontrada</h2></div>} />
       </Routes>
+
+      <Rodape />
     </BrowserRouter>
   );
 }
